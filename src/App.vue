1f5a5ccd2a2b1a298efdd7e81c70a0ae73064ff6
LinXiNotDream <1113525779@qq.com>
2018-01-13 23:53:29 +0800

    canvas绘制

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <template>
   <div id="app">
-    <my-header></my-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import MyHeader from './components/MyHeader'
 
 export default {
-  name: 'app',
-  methods: {
-  },
-  components: {
-    MyHeader
-  }
 }
 </script>
 
---
+++ src/pages/persional/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <iframe src="../../../static/canvas/canvas.html" frameborder="0"></iframe>
+</template>
+
+<script>
+ export default {
+   data () {
+     return {
+
+     }
+   },
+   components: {
+
+   }
+ }
+</script>
+
+<style scoped>
+
+</style>
