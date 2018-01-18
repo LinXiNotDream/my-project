18f50720d277317b1dc9d76bf753f3857ef392d4
LinXiNotDream <30396000+LinXiNotDream@users.noreply.github.com>
2018-01-18 20:37:50 +0800

    Create home.vue

---
+++ src/pages/persional/home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <MyHeader></MyHeader>
+  </div>
+</template>
+
+<script>
+import MyHeader from '@/components/MyHeader'
+
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {
+    MyHeader
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
