43ea0e5cc5ab3a2346c3f045e631723bae25046e
LinXiNotDream <30396000+LinXiNotDream@users.noreply.github.com>
2018-01-18 20:38:20 +0800

    Update MyHeader.vue

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,13 +1,9 @@
 <template>
-  <el-menu class="el-menu-demo" :default-active="activeIndex" @select="handleSelect" router mode="horizontal" theme="dark">
-    <!-- 有二级菜单 -->
-    <el-submenu v-for="item in navBar" v-if="item.children" :key="item.name" :index="item.index" v-show="item.show">
-      <template slot="title">{{item.name}}</template>
-      <el-menu-item v-for="child in item.children" :key="child.name" :index="child.index">{{item.name}}</el-menu-item>
-    </el-submenu>
-    <!-- 没有二级菜单 -->
-    <el-menu-item v-else :index="item.index" v-show="item.show">{{item.name}}</el-menu-item>
-  </el-menu>
+  <div class="header">
+    <div class="title" v-for="item in navBar" :key="item.name">
+      <span>{{item.name}}</span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -17,16 +13,13 @@ export default {
       activeIndex: '/persional/index',
       navBar: [{
         name: '首页',
-        index: '/persional/index',
-        show: true
+        path: '/persional/index'
       }, {
         name: '干货分享',
-        index: '/persional/blog',
-        show: true
+        path: '/persional/blog'
       }, {
         name: '个人中心',
-        index: '/persional/center',
-        show: true
+        path: '/persional/center'
       }]
     }
   },
