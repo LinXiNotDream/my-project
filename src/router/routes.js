64cc39740ec29ef08e32e00b2cf3b98d423afcd3
LinXiNotDream <30396000+LinXiNotDream@users.noreply.github.com>
2018-01-18 17:36:42 +0800

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,23 @@
 import canvas from '@/pages/canvas/index'
 import elec from '@/pages/checkElec/Electric'
 
-var routes = [{
-  path: '/',
-  name: 'canvas',
-  component: canvas
-}, {
-  path: '/canvas',
-  name: 'canvas',
-  component: canvas
-}, {
-  path: '/elec',
-  name: 'elec',
-  component: elec
-}]
+import HomePage from './home'
+
+var routes = [
+  {
+    path: '/',
+    name: 'canvas',
+    component: canvas
+  }, {
+    path: '/canvas',
+    name: 'canvas',
+    component: canvas
+  }, {
+    path: '/elec',
+    name: 'elec',
+    component: elec
+  },
+  ...HomePage
+]
 
 export default routes
