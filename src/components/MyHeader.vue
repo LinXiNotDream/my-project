9ff9973c1625550bf216e37a6a30617912eb51d8
LinXiNotDream <30396000+LinXiNotDream@users.noreply.github.com>
2018-01-19 17:31:40 +0800

    Update MyHeader.vue

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="header">
-    <div class="title" v-for="item in navBar" :key="item.name">
-      <span>{{item.name}}</span>
+    <div class="box">
+      <div class="title" v-for="item in navBar" :class="{active: item.isActive}" :key="item.name" @click="pageChange(item.path, item.path)">
+        <span>{{item.name}}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -10,29 +12,60 @@
 export default {
   data () {
     return {
-      activeIndex: '/persional/index',
-      navBar: [{
-        name: '首页',
-        path: '/persional/index'
-      }, {
-        name: '干货分享',
-        path: '/persional/blog'
-      }, {
-        name: '个人中心',
-        path: '/persional/center'
-      }]
+      navBar: [
+        {
+          name: '首页',
+          path: '/persional/index',
+          isActive: false
+        }, {
+          name: '干货分享',
+          path: '/persional/blog',
+          isActive: false
+        }, {
+          name: '个人中心',
+          path: '/persional/center',
+          isActive: false
+        }
+      ]
     }
   },
   methods: {
-    handleSelect (key, keyPath) {
-      console.log(key, keyPath)
+    pageChange (pageName, pagePath) {
+      let navBar = this.navBar
+      navBar.forEach(nav => {
+        nav.isActive = nav.name === pageName
+      })
+      this.$router.push(pagePath)
     }
   }
 }
 </script>
 
 <style scoped>
+.header {
+  height: 60px;
+  background: #24292e;
+}
+.box {
+  width: 1200px;
+  margin: 0 auto;
+}
 .title {
   display: inline-block;
+  width: 130px;
+  height: 100%;
+  line-height: 60px;
+  text-align: center;
+  font-size: 20px;
+  cursor: pointer;
+  color: #fff;
+}
+.title:hover {
+  color: #f6f9d3;
+  background: #3c4040;
+}
+.active {
+  color: #f6f9d3;
+  background: #3c4040;
 }
 </style>
