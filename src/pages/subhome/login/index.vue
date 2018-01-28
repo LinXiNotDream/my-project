217ce61a4c20c6660bf949587d24acaf9a804006
LinXiNotDream <1113525779@qq.com>
2018-01-28 21:52:53 +0800

    登录模块功能完善

--- src/pages/subhome/login/index.vue
+++ src/pages/subhome/login/index.vue
@@ -2,8 +2,15 @@
   <div>
     <MyHeader></MyHeader>
     <div class="box">
-      <div>
-
+      <div class="login-box">
+        <div class="username">
+          <span>用户名</span>
+          <el-input prefix-icon="el-icon-search"></el-input>
+        </div>
+        <div class="password">
+          <span>密码</span>
+          <el-input prefix-icon="el-icon-search"></el-input>
+        </div>
       </div>
     </div>
   </div>
@@ -27,5 +34,25 @@ export default {
   background: url(../../../assets/bgimg/0416.jpg) no-repeat;
   background-position: 100% 34%;
   background-size: 1400px;
+}
+.login-box {
+  width: 320px;
+  height: 300px;
+  position: absolute;
+  top: 35%;
+  right: 24%;
+  padding: 20px;
+  border: 1px solid;
+}
+.login-box div {
+  margin-bottom: 10px;
+}
+.login-box div span {
+  display: inline-block;
+  width: 55px;
+  text-align-last: justify;
+}
+.el-input {
+  width: 250px;
 }
 </style>
