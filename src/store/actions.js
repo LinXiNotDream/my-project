db3e23581bb503630492da730101e2333fb6298b
HallRain <30396000+LinXiNotDream@users.noreply.github.com>
2018-02-01 17:00:23 +0800

    Update actions.js

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,11 +4,13 @@ export const do_login = ({ commit, state }) => {
   return new Promise((resolve, reject) => {
     // 登录方法
     commit(types.LOGIN)
+    resolve()
   })
 }
 
 export const do_logout = ({ commit, state }) => {
   return new Promise((resolve, reject) => {
     commit(types.LOGIN_OUT)
+    resolve()
   })
 }
