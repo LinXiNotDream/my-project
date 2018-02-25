4e271f97706bea190dc9af512557a37fdfc1c2c5
LinXiNotDream <1113525779@qq.com>
2018-02-25 22:13:39 +0800

    学习三栏布局

--- src/views/subhome/blog/index.vue
+++ src/views/subhome/blog/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="box">
-    test
+    <div class="left"></div>
+    <div class="right"></div>
+    <div class="main"></div>
   </div>
 </template>
 
@@ -16,5 +18,22 @@ export default {
 </script>
 
 <style scoped>
-
+.left {
+  float: left;
+  height: 200px;
+  width: 100px;
+  background-color: red;
+}
+.right {
+  width: 200px;
+  height: 200px;
+  background-color: blue;
+  float: right;
+}
+.main {
+  margin-left: 120px;
+  margin-right: 220px;
+  height: 200px;
+  background-color: green;
+}
 </style>
