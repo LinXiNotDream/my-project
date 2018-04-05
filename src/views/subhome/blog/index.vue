c17c7c7303549e2ff16bdd5989c7ce06b794b437
LinXiNotDream <1113525779@qq.com>
2018-04-05 19:03:16 +0800

    小改动

--- src/views/subhome/blog/index.vue
+++ src/views/subhome/blog/index.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="box">
-    <div class="container">
-      <div class="main"></div>
-      <div class="left"></div>
-      <div class="right"></div>
-    </div>
   </div>
 </template>
 
@@ -20,26 +15,5 @@ export default {
 </script>
 
 <style scoped>
-.container {
-  display: flex;
-}
-.left {
-  order: -1;
-  flex: 0 1 200px;
-  margin-right: 20px;
-  height: 200px;
-  background-color: red;
-}
-.right {
-  order: 2;
-  flex: 0 1 100px;
-  height: 200px;
-  margin-left: 10px;
-  background-color: blue;
-}
-.main {
-  flex-grow: 1;
-  height: 200px;
-  background-color: green;
-}
+
 </style>
