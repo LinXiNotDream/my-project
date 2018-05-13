2e03eeea123ee5e94f3e50e4a40698b65b8c490c
LinXiNotDream <1113525779@qq.com>
2018-05-13 22:40:32 +0800

    vue-corppa功能完善

--- src/views/subhome/blog/index.vue
+++ src/views/subhome/blog/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="box">
-    <image-croppa v-model="myCroppa" :prevent-white-space="true" :width="croppaWidth" :height="croppaHeight"></image-croppa>
+    <image-croppa v-model="myCroppa" :prevent-white-space="true" :width="croppaWidth" :height="croppaHeight" @init="onInit">
+    </image-croppa>
+    <button @click="upload">上传</button>
   </div>
 </template>
 
@@ -10,7 +12,7 @@ export default {
     return {
       myCroppa: {},
       croppaWidth: 400,
-      croppaHeight: 250
+      croppaHeight: 400
     }
   },
   methods: {
@@ -18,6 +20,25 @@ export default {
       this.myCroppa.generateBlob((blob) => {
          // write code to upload the cropped image file (a file is a blob)
       }, 'image/jpeg', 0.8) // 80% compressed jpeg file
+    },
+    onInit () {
+      this.myCroppa.addClipPlugin(function (ctx, x, y, w, h) {
+        ctx.beginPath()
+        ctx.arc(x + w / 2, y + h / 2, w / 2, 0, 2 * Math.PI, true)
+        ctx.closePath()
+      })
+    },
+    upload () {
+      if (!this.myCroppa.hasImage()) {
+        alert('no image to upload')
+        return
+      }
+      this.myCroppa.generateBlob((blob) => {
+        var fd = new FormData()
+        fd.append('file', blob)
+        fd.append('other', 'blahblahblah')
+        console.log(blob)
+      })
     }
   }
 }
