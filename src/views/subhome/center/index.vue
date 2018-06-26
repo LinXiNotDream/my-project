c774d4edcefc9aff0a134717a71923615572e001
LinXiNotDream <1113525779@qq.com>
2018-06-26 23:34:41 +0800

    联想输入

--- src/views/subhome/center/index.vue
+++ src/views/subhome/center/index.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
-    <span @click="open">开启</span>
+  <div @click.stop>
+    <!-- <span @click="open">开启</span>
     <lww-dialog :visible.sync="showDialog" :width="300" :showTitle="true" :title="'Test'"></lww-dialog>
-    <lww-input></lww-input>
+    <lww-input></lww-input> -->
+    <input ref="input" v-model="schoolName" @input="getSuggestion(schoolName)" />
+    <div class="school-suggestion" v-show="showSuggestion">
+      <ul>
+        <li v-for="(item, index) in schoolSuggestions" :key="index" @click="setName(item.name)">{{item.name}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -11,12 +17,45 @@ export default {
   components: {},
   data () {
     return {
-      showDialog: false
+      showDialog: false,
+      schoolName: '',
+      showSuggestion: false,
+      schoolSuggestions: [],
+      isSelect: false
     }
+  },
+  mounted () {
+    let old = document.onclick || (() => {})
+    document.addEventListener('click', () => {
+      old()
+      this.showSuggestion = false
+    })
   },
   methods: {
     open () {
       this.showDialog = true
+    },
+    getSuggestion (val) {
+      let str = this.schoolName
+      if (val.length) {
+        this.schoolSuggestions = [{name: '111+'}, {name: '22222+'}, {name: str + 'hehe'}]
+        if (this.isSelect) {
+          this.isSelect = false
+        } else {
+          this.showSuggestion = true
+        }
+      }
+    },
+    loseSuggestion () {
+      setTimeout(console.log('aaa'), 2000)
+      this.showSuggestion = false
+      console.log('bbb')
+    },
+    setName (name) {
+      this.isSelect = true
+      this.schoolName = name
+      this.showSuggestion = false
+      console.log('setName')
     }
   }
 }
